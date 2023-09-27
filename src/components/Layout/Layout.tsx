fd38cd57b51deba0f1ba0ebabc1ce852590442d8
Joshua Burns <joshua.burns@liatrio.com>
2023-09-27 08:26:24 -0700

    changed image to wildcat

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -18,8 +18,8 @@ const Layout = ({ children }: Props) => (
           <a className={styles.navbarBrand}>
             <span className={globalStyles.logo}>
               <Image
-                src="/dojo-logo-sm.png"
-                // src="/wildcat.png"
+                //src="/dojo-logo-sm.png"
+                src="/wildcat.png"
                 alt="dojo-small"
                 width={50}
                 height={50}
